e1e32fb1203df37ce100b612629a30d23d064bab
Rohit Vemuri <rvemuri2@ucmerced.edu>
2023-09-12 10:34:32 -0700

    added scheduled blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,9 +3,10 @@ class BlogPostsController < ApplicationController
     before_action :set_blog_post, except: [:index, :new, :create] #only: [:show, :update, :edit, :destory]
 
     def index
-        @blog_posts = BlogPost.all
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published
     end
     def show
+        
     end
     
     def new 
@@ -40,11 +41,11 @@ class BlogPostsController < ApplicationController
 
     private 
     def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
+        params.require(:blog_post).permit(:title, :body, :published_at)
     end
 
     def set_blog_post
-        @blog_post = BlogPost.find(params[:id])
+        @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path 
     end
